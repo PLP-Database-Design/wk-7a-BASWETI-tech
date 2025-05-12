6d52572cef0a4ad6c5defce5e3376711b21add76
Basweti <72159438+BASWETI-tech@users.noreply.github.com>
2025-05-12 12:34:26 +0300

    Add files via upload
    week7 assignments

---
+++ question1.sql
@@ -0,0 +1,5 @@
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(100)
+);
---
+++ question2.sql
@@ -0,0 +1,12 @@
+ -- CREATE TABLE Orders1 (
+	-- OrderID INT PRIMARY KEY,
+    -- CustomerName VARCHAR(100)
+    -- );
+-- INSERT INTO orders1 (OrderID, CustomerName)
+-- SELECT DISTINCT OrderID, CustomerName
+-- FROM OrderDetails;
+-- INSERT INTO OrderProducts (OrderID, Product, Quantity)
+-- SELECT OrderID, Product, Quantity
+-- FROM OrderDetails;
+
+
